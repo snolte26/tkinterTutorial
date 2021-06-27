1b786efa7c0926699ba1a65305eb486e315fe241
Alexander Nolte <35269769+snolte26@users.noreply.github.com>
2021-06-26 23:21:54 -0400

    Add files via upload

---
+++ labels.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+from tkinter import ttk
+
+root = tk.Tk()
+root.geometry('300x200')
+root.resizable(True, True)
+root.title('Label Widget Demo')
+root.wm_minsize(300, 200)
+
+# show the label here
+label = ttk.Label(root, text='Label with Helvetica size 14 font', font=("Helvetica", 14))
+label.pack(ipadx=10, ipady=10)
+
+# photo
+photo = tk.PhotoImage(file='fbi_logo.png')
+imageLabel = ttk.Label(
+    root,
+    image=photo,
+    padding=5,
+    text='FBI Logo',
+    compound='top'
+)
+imageLabel.pack()
+
+root.mainloop()
